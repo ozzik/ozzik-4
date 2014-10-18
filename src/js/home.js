44c34d73c102e1afb1aec20d022bc81dea1f40ca
Oz Pinhas <hey@ozzik.co>
2014-10-18 20:18:10 +0300

    - Return of the navline - Fixes justified text

--- src/js/home.js
+++ src/js/home.js
@@ -17,26 +17,26 @@ var Home = {};
 
             Navline.eHeader.on("mouseover", Navline.handle_mouseover);
             Navline.eHeader.on("mouseout", Navline.handle_mouseout);
-            Navline.handle_mouseover({ target: Navline.eActive });
+            Navline.handle_mouseover({ target: Navline.eActive }, true);
         },
 
         update_offset: function() {
             Navline.offsetX = Navline.eHeader[0].offsetLeft;
         },
 
-        handle_mouseover: function(e) {
+        handle_mouseover: function(e, isRevert) {
             // Suppression
             if (!e.target || e.target.nodeName !== "A") { return; }
 
             Navline.itemWidth = e.target.offsetWidth;
 
             Navline.e[0].style.width = Navline.itemWidth + "px";
-            Navline.e.transform("translate3d(" + (e.target.offsetLeft - Navline.offsetX) + "px,0,0) scaleX(.05)");
+            Navline.e.transform("translate3d(" + (e.target.offsetLeft - Navline.offsetX) + "px,0,0) " + (!isRevert ? "scaleX(.05)" : ""));
         },
 
         handle_mouseout: function(e) {
             if (e.relatedTarget.nodeName === "UL") { return; };
-            Navline.handle_mouseover({ target: Navline.eActive });
+            Navline.handle_mouseover({ target: Navline.eActive }, true);
         }
     }
 
