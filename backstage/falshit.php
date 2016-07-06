a2db5227021d250f527737016e6b3e218c21ab89
Oz Pinhas <hey@ozzik.co>
2016-07-06 08:04:21 +0300

    Tiny fixes
    - Fixes contact items being broken in about section
    - Fixes ignore cookie thingy

--- backstage/falshit.php
+++ backstage/falshit.php
@@ -2,12 +2,12 @@
 	include_once("config.php");
 
 	if (isset($_GET['check'])) {
-		var_dump($_COOKIE[$_ME_COOKIE]);
+		var_dump($_COOKIE[_ME_COOKIE]);
 	} elseif (isset($_GET['no'])) {
-		setcookie($_ME_COOKIE, "", 1);
+		setcookie(_ME_COOKIE, "", 1);
 		echo "no more cookie for you";
 	} else {
-		setcookie($_ME_COOKIE, "fosho", time() + (10 * 365 * 24 * 60 * 60));
+		setcookie(_ME_COOKIE, "fosho", time() + (10 * 365 * 24 * 60 * 60));
 		var_dump("lots of COOKIEzz for you");
 	}
 ?>
